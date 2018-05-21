7fc29744a8ac6912089315eb7af5e8d316a3999c
Farabi <farabi101@gmail.com>
2018-05-21 04:31:05 +0000

    task 2

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 /* JavaScript code here */
+//task 1
+console.log("Extarnal JS")
+
+//Task 2
+function sumArray(arr){
+    var sum = 0;
+    arr.forEach(function(value, index){
+        sum+=value;
+    });
+    return sum;
+}
+console.log(sumArray([1,2,3,4,5]));
+console.log(sumArray([1,20,-3,45]));
